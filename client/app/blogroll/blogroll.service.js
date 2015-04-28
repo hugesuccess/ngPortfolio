e75f4b9a10f51823ded6592aa030ef91a725426b
Victor Venters <victor@huge-success.net>
2015-04-28 16:46:54 -0700

    Revised Blogroll Service -- hard coded JSON for now

--- client/app/blogroll/blogroll.service.js
+++ client/app/blogroll/blogroll.service.js
@@ -3,8 +3,39 @@
 angular.module('hugesuccessApp')
   .service('blogroll', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
+    var br = [
+        {
+            "title": "EventedMind",
+            "link": "http://www.eventedmind.com"
+        },
+        {
+            "title": "Badass JavaScript",
+            "link": "http://www.badassjs.com"
+        },
+        {
+            "title": "Meteor",
+            "link": "http://www.meteor.com"
+        },
+        {
+            "title": "NCZOnline",
+            "link": "http://www.nczonline.net"
+        },
+        {
+            "title": "Crockford",
+            "link": "http://www.javascript.crockford.com"
+        },
+        {
+            "title": "How to Node",
+            "link": "http://www.howtonode.org"
+        },
+        {
+            "title": "Oaklandjs",
+            "link": "http://www.oaklandjs.com"
+        }
+    ];
     return {
         get: function(cb) {
+            return cb(null, br);
             $http.get("assets/json/blogroll.json", {cache: true})
                 .error(function(err) {
                     return cb(err, null);
