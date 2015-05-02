2cf15da5c81c295832ea2381c7cd2c4c1b8d76f2
Victor Venters <victor@huge-success.net>
2015-05-02 10:09:35 -0700

    Updated injected HTML bug

--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -4,5 +4,6 @@ angular.module('hugesuccessApp')
   .controller('ProjectCtrl', function ($scope, $stateParams, projects) {
       projects.getOne($stateParams.id, function(err, data) {
         $scope.project = data;
+        $scope.copy = data.description;
     })
   });
