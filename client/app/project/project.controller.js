660111f25d64fc5f30184fe5080e53a297dbf2ee
Victor Venters <victor@huge-success.net>
2015-05-02 10:18:37 -0700

    Revised HTML bind

--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -3,6 +3,7 @@
 angular.module('hugesuccessApp')
   .controller('ProjectCtrl', function ($scope, $stateParams, projects) {
       projects.getOne($stateParams.id, function(err, data) {
+      console.log(data)
         $scope.project = data;
         $scope.copy = data.description;
     })
